af2ddaa812ca0f4992b33c6cefd9b78505838c2a
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-22 13:53:56 +0200

    Add variables for name, surname and fav color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 // Ask user name
-
+var userName = prompt("What's your name?")
 // Ask user surname
-
+var userSurname = prompt("What's your surname?")
 // Ask user favourite color
-
+var favColor = prompt("What's your favorite color?")
 // Print in HTML: name+surname+color+21
+document.getElementById('password').innerHTML = userName + userSurname + favColor + "21"
