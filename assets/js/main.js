98d30b4de01ed805e08ae37dcb55958b3d0fa28a
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-22 13:58:56 +0200

    add var for final password, used getelemntbyid to print it in lowercase

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,5 @@ var userSurname = prompt("What's your surname?")
 // Ask user favourite color
 var favColor = prompt("What's your favorite color?")
 // Print in HTML: name+surname+color+21
-document.getElementById('password').innerHTML = userName + userSurname + favColor + "21"
+var finalPassword = userName + userSurname + favColor + "21"
+document.getElementById('password').innerHTML = finalPassword.toLowerCase()
